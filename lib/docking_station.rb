4cad1c24d0fe1f3ba5575fa89b3bc82e9cc5b6f6
Fabio <alessf13@gmail.com>
2021-05-27 16:09:26 +0100

    private methods full? and empty? created

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,14 +8,24 @@ class DockingStation
     end
   
     def release_bike
-      unless @bike_no.length > 0
+      unless empty?
         fail 'No bikes available!' 
       end
       @bike_no.pop
     end   
   
     def dock(bike)
-      raise 'Docking station full' if @bike_no.length >= 20
+      raise 'Docking station full' if full?
       @bike_no << bike
+    end
+
+    private
+
+    def full?
+      @bike_no.length >= 20 ? true : false
+    end
+
+    def empty?
+      @bike_no.length > 0 ? true : false
     end
   end
